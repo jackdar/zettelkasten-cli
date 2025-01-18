d7a52fb1af9868fab8131a0f1b8c42f31c5e3c96
Jack Darlington <100313566+jackdar@users.noreply.github.com>
2025-01-18 14:49:36 +1300

    feat: implement init cmd to quickly scaffold directory structure

--- cmd/init.go
+++ cmd/init.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -11,7 +13,33 @@ var initCmd = &cobra.Command{
 	Short: "Initialize Zettelkasten directory",
 	Long:  "Initializes the home Zettlekasten directory (default is $HOME/zettlekasten)",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Initializing!\n")
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("Error getting home directory:", err)
+			return
+		}
+
+		if _, err := os.ReadDir(filepath.Join(home, "Zettelkasten")); err == nil {
+			fmt.Println("Zettelkasten directory already exists!")
+			return
+		}
+
+		dirs := []string{
+			filepath.Join(home, "Zettelkasten"),
+			filepath.Join(home, "Zettelkasten", "__Inbox"),
+			filepath.Join(home, "Zettelkasten", "_Zettelkasten"),
+			filepath.Join(home, "Zettelkasten", "Templates"),
+		}
+
+		for _, dir := range dirs {
+			err := os.MkdirAll(dir, 0755)
+			if err != nil {
+				fmt.Println("Error creating directory:", err)
+				return
+			}
+		}
+
+		fmt.Println("Zettelkasten directory structure initialized successfully!")
 	},
 }
 
