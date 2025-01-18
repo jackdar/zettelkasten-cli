3a8fac4699ad27fa7a85acbbcda8c0a46cb06dba
Jack Darlington <100313566+jackdar@users.noreply.github.com>
2025-01-18 14:49:03 +1300

    feat: implement list cmd to show today's created notes

--- cmd/list.go
+++ cmd/list.go
@@ -3,41 +3,42 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
-	"github.com/spf13/viper"
+	"github.com/spf13/cobra"
 
 	"github.com/jackdar/zettelkasten-cli/internal/utils"
 )
 
-func init() {
-
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List notes created on the same day",
+	Run: func(cmd *cobra.Command, args []string) {
+		listNotes()
+	},
 }
 
-func initDirectory() {
-	dirName := viper.GetString("ZKDIR")
-	dir, err := os.ReadDir(dirName)
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
 
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Zettelkasten directory not found! Creating...\n")
+func listNotes() {
+	dir, err := os.ReadDir(filepath.Join(conf.ZkDir, "__Inbox"))
+	utils.CheckErr(err)
 
-		err := createDirectory(dirName)
-		utils.CheckErr(err)
-	}
+	today := time.Now().Format("2006-01-02")
 
 	for _, entry := range dir {
-		fmt.Printf(" - %s\n", entry.Name())
+		if entry.IsDir() {
+			continue
+		}
+
+		info, err := entry.Info()
+		utils.CheckErr(err)
+
+		if info.ModTime().Format("2006-01-02") == today {
+			fmt.Printf(" - %s\n", entry.Name())
+		}
 	}
-}
-
-func createDirectory(dir string) error {
-	err := os.Mkdir(dir, 0755)
-	utils.CheckErr(err)
-
-	err = os.Mkdir(dir+"/__Inbox", 0755)
-	utils.CheckErr(err)
-
-	err = os.Mkdir(dir+"/_Zettelkasten", 0755)
-	utils.CheckErr(err)
-
-	return err
 }
